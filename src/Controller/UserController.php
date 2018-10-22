3a1222d464a0d30882b6120185758a1808c3a3e8
csillagpatrik32 <csillagpatrik@gmail.com>
2018-10-22 23:10:22 +0200

    Basic setup: next step to store token in DB

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Catman
+ * Date: 2018. 10. 22.
+ * Time: 13:46
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UserController extends Controller
+{
+
+}
